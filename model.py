3d44c3aa4990871d4b04f906f4b3e8d3ace42afd
StanJohn04 <stanjohn04@gmail.com>
2023-08-03 20:02:50 -0600

    html

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ from keras.models import load_model
 app = Flask(__name__)
 
 # Load the pre-trained model
-model = load_model(r"C:\Users\Brian Haynes\Desktop\Cat_or_Dog\CNN_Model\cat_or_dog_CNN.keras")
+model = load_model("CNN_Model\cat_or_dog_CNN.keras")
 
 # Define the process_image() function
 def process_image(image):
@@ -19,15 +19,15 @@ def process_image(image):
     img = img.reshape(1, 150, 150, 1)
     return img
 
-@app.route('cat_dog', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        file = request.files['file']
+        file = request.files['image_file']
         if file:
             image = process_image(file)
             prediction = model.predict(image)
             result = 'Dog' if prediction[0][0] >= 0.5 else 'Cat'
-            return render_template('result.html', result=result)
+            return render_template('index.html', result=result)
     return render_template('index.html')
 
 if __name__ == '__main__':
